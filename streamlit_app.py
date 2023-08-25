76eff55c5c514a51e37d7c77421aeefbf44994ae
Viperdrone7777 <107051438+Viperdrone7777@users.noreply.github.com>
2023-08-25 14:49:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,9 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+
+
+
+
+
